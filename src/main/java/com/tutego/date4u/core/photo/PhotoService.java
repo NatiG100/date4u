6cfcccd4d7575b1d2af53da1b89670cda4e63208
NatiGK <natnaelgashu2022@gmail.com>
2024-02-27 15:44:09 +0300

    get list of beans

--- src/main/java/com/tutego/date4u/core/photo/PhotoService.java
+++ src/main/java/com/tutego/date4u/core/photo/PhotoService.java
@@ -2,8 +2,10 @@ package com.tutego.date4u.core.photo;
 
 import java.io.UncheckedIOException;
 import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tutego.date4u.core.FileSystem;
@@ -11,11 +13,11 @@ import com.tutego.date4u.core.FileSystem;
 @Service
 public class PhotoService {
     private final FileSystem fs;
-    private final Thumbnail thumbnail;
+    private final List<Thumbnail> thumbnails;
 
-    public PhotoService(FileSystem fs, Thumbnail thumbnail) {
+    public PhotoService(FileSystem fs, @Autowired List<Thumbnail> thumbnails) {
         this.fs = fs;
-        this.thumbnail = thumbnail;
+        this.thumbnails = thumbnails;
     }
 
     public Optional<byte[]> download(String name) {
@@ -26,13 +28,13 @@ public class PhotoService {
         }
     }
 
-    public String upload(byte[] imageBytes){
+    public String upload(byte[] imageBytes) {
         String imageName = UUID.randomUUID().toString();
         // First: store original image
         fs.store(imageName + ".jpg", imageBytes);
         // Second: store thumbnail
-        byte[] thumbnailBytes = thumbnail.thumbnail(imageBytes);
-        fs.store(imageName+"-thumb.jpg", thumbnailBytes);
+        byte[] thumbnailBytes = thumbnails.get(0).thumbnail(imageBytes);
+        fs.store(imageName + "-thumb.jpg", thumbnailBytes);
         return imageName;
     }
 }
