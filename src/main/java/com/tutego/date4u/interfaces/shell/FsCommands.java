c2ec6fdfe3f3706b8bcf015af122bf50fc056ddd
NatiGK <natnaelgashu2022@gmail.com>
2024-02-29 08:47:22 +0300

    environment enjection

--- src/main/java/com/tutego/date4u/interfaces/shell/FsCommands.java
+++ src/main/java/com/tutego/date4u/interfaces/shell/FsCommands.java
@@ -1,5 +1,8 @@
 package com.tutego.date4u.interfaces.shell;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.util.unit.DataSize;
@@ -9,16 +12,29 @@ import com.tutego.date4u.core.FileSystem;
 @ShellComponent
 public class FsCommands {
 
-    // private final FileSystem fs = new FileSystem();
-
+    @Value("${date4u.filesystem.minimum-free-disk-space}")
+    private long minimumFreeDiskSpace;
+    @Autowired
+    private Environment env;
     private final FileSystem fs;
 
     public FsCommands(FileSystem fs) {
         this.fs = fs;
+
+    }
+
+    @ShellMethod("Display user home")
+    public String userHome() {
+        return env.getProperty("user.home");
     }
 
     @ShellMethod("Display free disk space")
     public String freeDiskSpace() {
         return DataSize.ofBytes(fs.getFreeDiskSpace()).toGigabytes() + "GB";
+    }
+
+    @ShellMethod("Display required free disk space")
+    public long minimumFreeDiskSpace() {
+        return minimumFreeDiskSpace;
     }
 }
