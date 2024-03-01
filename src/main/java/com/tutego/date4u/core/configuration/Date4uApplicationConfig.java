e57ace1dc9db58a48a35bb59eec51ac6ad1decb3
NatiGK <natnaelgashu2022@gmail.com>
2024-03-01 15:10:15 +0300

    profiles

--- src/main/java/com/tutego/date4u/core/configuration/Date4uApplicationConfig.java
+++ src/main/java/com/tutego/date4u/core/configuration/Date4uApplicationConfig.java
@@ -1,26 +1,47 @@
 package com.tutego.date4u.core.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("date4u")
 public class Date4uApplicationConfig {
-    public static class Filesystem{
+
+    record Server(String ip) {
+    }
+
+    private List<Server> servers = new ArrayList<>();
+
+    public List<Server> getServers() {
+        return servers;
+    }
+
+    public void setServers(List<Server> servers) {
+        this.servers = servers;
+    }
+
+    public static class Filesystem {
         private long minimumFreeDiskSpace = 1_000_000;
-        public long getMinimumFreeDiskSpace(){
+
+        public long getMinimumFreeDiskSpace() {
             return minimumFreeDiskSpace;
         }
-        public void setMinimumFreeDiskSpace(long minimumFreeDiskSpace){
+
+        public void setMinimumFreeDiskSpace(long minimumFreeDiskSpace) {
             this.minimumFreeDiskSpace = minimumFreeDiskSpace;
         }
     }
 
     private Filesystem filesystem = new Filesystem();
-    public Filesystem getFilesystem(){
+
+    public Filesystem getFilesystem() {
         return filesystem;
     }
-    public void setFilesystem(Filesystem filesystem){
+
+    public void setFilesystem(Filesystem filesystem) {
         this.filesystem = filesystem;
     }
 }
